fd14dd122906e027f9dc2f19aa8710034392b893
Brian Espinosa <b@bje.co>
2018-07-18 09:52:08 -0700

    Fix to work with global install and drastically improved logging with partyparrot

--- index.js
+++ index.js
@@ -5,15 +5,24 @@
 const program = require('commander');
 const chalk = require('chalk');
 const bs = require('browser-sync').create();
+const globalModules = require('global-modules');
+const parrotSay = require('parrotsay-api');
 
 program
   .arguments('<file>')
   .action((file) => {
-    console.log(chalk.bold.green('Generating slides for file: ') + file);
+    if (file) {
+      parrotSay(chalk.bold.green(' Generating slides for file: ') + chalk.bold.cyan(file) + ' ')
+        .then(console.log)
+        .catch(console.error)
+    }
+    else {
+      console.error(chalk.bold.red('💩 You need to provide a filename for this to work.'));
+    }
 
     bs.init({
       serveStatic  : ['.'],
-      server       : './node_modules/esp-slide/app',
+      server       : globalModules + '/@espressive/esp-slide/app',
       watch        : true,
       files        : ['*.md','*.css'],
       rewriteRules : [
